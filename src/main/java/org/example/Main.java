4d9149da80b3d7adc37e3874a9f697adb8db78c2
Joe Wang <joewa@google.com>
2023-11-15 11:47:27 -0500

    build get request in infinite loop

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,6 @@ package org.example;
 
 import com.google.api.client.http.GenericUrl;
 import com.google.api.client.http.HttpRequestFactory;
-import com.google.api.client.http.HttpResponse;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import java.io.IOException;
 
@@ -11,11 +10,8 @@ public class Main {
   public static void main(String[] args) throws IOException {
     NetHttpTransport netHttpTransport = new NetHttpTransport();
     HttpRequestFactory requestFactory = netHttpTransport.createRequestFactory();
-    HttpResponse response = requestFactory
-        .buildGetRequest(new GenericUrl("https://www.google.com"))
-        .execute();
-    System.out.println(response.getStatusCode());
-    System.out.println(response.setLoggingEnabled(true));
-    System.out.println(response.getContentType());
+    while (true) {
+      requestFactory.buildGetRequest(new GenericUrl("https://www.google.com"));
+    }
   }
 }
